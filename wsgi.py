0afcafc183bddd254637438d00946a9e800b4150
w0rp <devw0rp@gmail.com>
2013-11-03 11:21:19 +0000

    Restructured the project to move things into the root directory, and now the blog is in an app of its own.

--- wsgi.py
+++ wsgi.py
@@ -19,7 +19,7 @@ import os
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "w0rpzone.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "w0rpzone.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
@@ -30,3 +30,4 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
