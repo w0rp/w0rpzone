53ddfd4a2d5d57b4c0c0df3c5789518be5f305d4
w0rp <devw0rp@gmail.com>
2014-02-22 13:48:50 +0000

    Make the article edit side navigation float when scrolling down the page, when viewing the page in medium or large sized browser windows. Remember the scrolling position and set it back when flipping between previewing and editing. This should make things a bit nicer. Move the main.js file into the base template for the blog so it can be used in more places later.

--- staticfiles/js/blog/edit.js
+++ staticfiles/js/blog/edit.js
@@ -4,17 +4,36 @@ $(function() {
     var $form = $('.post_edit form:first');
     var $article = $('.post_edit article.preview');
     var $toggle_preview_button = $('.toggle_preview_button');
+    var $main = $("#main");
+    var $edit_nav = $main.children("nav");
+    var original_nav_top = $edit_nav.offset().top;
+    var nav_fixed = false;
+    var resize_timeout_handle = null;
+    var form_scroll_top = 0;
+    var preview_scroll_top = 0;
 
     function switch_to_preview() {
+        // Remember where we had scrolled to.
+        form_scroll_top = document.documentElement.scrollTop;
+
         $toggle_preview_button.addClass("toggled");
         $form.hide();
         $article.show();
+
+        // Set back where we had scroll to.
+        // We'll do this so we're looking at roughly the same thing
+        // as we flip preview on and off.
+        document.documentElement.scrollTop = preview_scroll_top;
     }
 
     function switch_to_form() {
+        preview_scroll_top = document.documentElement.scrollTop;
+
         $toggle_preview_button.removeClass("toggled");
         $article.hide();
         $form.show();
+
+        document.documentElement.scrollTop = form_scroll_top;
     }
 
     function generate_preview() {
@@ -51,11 +70,45 @@ $(function() {
         });
     }
 
+    function adjust_fixed_nav_horizontal() {
+        if (!nav_fixed) {
+            return;
+        }
+
+        // The left margin is the same size as the right margin.
+        $edit_nav.css("right", $main.offset().left);
+    }
+
     $toggle_preview_button.click(function() {
         if ($toggle_preview_button.hasClass("toggled")) {
             switch_to_form();
         } else {
             generate_preview();
         }
+    });
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > original_nav_top - 10) {
+            if (!nav_fixed) {
+                nav_fixed = true;
+
+                $edit_nav.addClass("fixed_when_big");
+                adjust_fixed_nav_horizontal();
+            }
+        } else {
+            if (nav_fixed) {
+                nav_fixed = false;
+
+                $edit_nav.removeClass("fixed_when_big");
+            }
+        }
+    });
+
+    $(window).resize(function(event) {
+        clearTimeout(resize_timeout_handle);
+
+        resize_timeout_handle = setTimeout(function() {
+            adjust_fixed_nav_horizontal();
+        }, 100);
     });
 });
