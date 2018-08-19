92b7ddab94657ee71a4c67a1270a07490fdf3a6e
w0rp <devw0rp@gmail.com>
2018-08-19 18:52:23 +0100

    Make markdown escaping safer, and support highlightjs better

--- w0rplib/templatetags/markdown.py
+++ w0rplib/templatetags/markdown.py
@@ -1,20 +1,11 @@
-import markdown2
-
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 
+import bleach
+import markdown2
+
 register = template.Library()
-
-
-@register.filter(is_safe=True)
-@stringfilter
-def markdown(value):
-    return mark_safe(markdown2.markdown(
-        text=value,
-        extras=["fenced-code-blocks", "code-friendly"],
-        safe_mode="escape",
-    ))
 
 
 # Not really safe, but Django needs to think it is.
@@ -23,5 +14,46 @@ def markdown(value):
 def unsafe_markdown(value):
     return mark_safe(markdown2.markdown(
         text=value,
-        extras=["fenced-code-blocks", "code-friendly"],
+        extras=[
+            "fenced-code-blocks",
+            "code-friendly",
+            "highlightjs-lang",
+        ],
     ))
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def markdown(value):
+    html = unsafe_markdown(value)
+
+    return bleach.clean(
+        html,
+        tags=[
+            'a',
+            'abbr',
+            'acronym',
+            'b',
+            'blockquote',
+            'code',
+            'em',
+            'i',
+            'li',
+            'ol',
+            'p',
+            'pre',
+            'strong',
+            'ul',
+        ],
+        attributes={
+            'a': ['href', 'title'],
+            'abbr': ['title'],
+            'acroynym': ['title'],
+            'code': ['class'],
+        },
+        protocols=[
+            'http',
+            'https',
+            'mailto',
+        ],
+    )
--- w0rplib/tests.py
+++ w0rplib/tests.py
@@ -1,7 +1,9 @@
+import textwrap
 import unittest
 
-from w0rplib.templatetags.text_formatting import word_break_on
 from w0rplib.compiler import CSSCompressor, RJSMinCompressor
+from w0rplib.templatetags.markdown import markdown, unsafe_markdown
+from w0rplib.templatetags.text_formatting import word_break_on
 
 
 class TemplateTagTestCase(unittest.TestCase):
@@ -42,3 +44,78 @@ class CompilerTestCase(unittest.TestCase):
             CSSCompressor(verbose=False).compress_css(test_css),
             "body{background:white}",
         )
+
+
+class MarkdownTestCase(unittest.TestCase):
+    def test_html_escaping(self):
+        # This value was used to demonstrate a flag with the markdown2 library.
+        text = '<<svg/onload=prompt(1) x'
+
+        self.assertEqual(
+            unsafe_markdown(text),
+            '<p>&lt;<svg/onload=prompt(1) x</p>\n',
+        )
+        self.assertEqual(
+            markdown(text),
+            '<p>&lt;&lt;svg onload="prompt(1)" p="" x&lt;=""&gt;'
+            + '\n&lt;/svg&gt;</p>'
+        )
+
+    def test_ordered_list_formatting(self):
+        text = textwrap.dedent(r'''
+        1. foo
+        2. bar
+        3. baz
+        ''')
+        html = textwrap.dedent(r'''
+        <ol>
+        <li>foo</li>
+        <li>bar</li>
+        <li>baz</li>
+        </ol>
+        ''').lstrip()
+
+        self.assertEqual(unsafe_markdown(text), html)
+        self.assertEqual(markdown(text), html)
+
+    def test_unordered_list_formatting(self):
+        text = textwrap.dedent(r'''
+        * foo
+        * bar
+        * baz
+        ''')
+        html = textwrap.dedent(r'''
+        <ul>
+        <li>foo</li>
+        <li>bar</li>
+        <li>baz</li>
+        </ul>
+        ''').lstrip()
+
+        self.assertEqual(unsafe_markdown(text), html)
+        self.assertEqual(markdown(text), html)
+
+    def test_emphasis(self):
+        text = '_foo_ __bar__ *foo* **bar**'
+        html = '<p>_foo_ __bar__ <em>foo</em> <strong>bar</strong></p>\n'
+
+        self.assertEqual(unsafe_markdown(text), html)
+        self.assertEqual(markdown(text), html)
+
+    def test_links(self):
+        text = '[foo](https://bar.com)'
+        html = '<p><a href="https://bar.com">foo</a></p>\n'
+
+        self.assertEqual(unsafe_markdown(text), html)
+        self.assertEqual(markdown(text), html)
+
+    def test_language_for_highlights_with_markdown_tags(self):
+        text = textwrap.dedent(r"""
+        ```js
+        const x = 1
+        ```
+        """)
+        html = '<pre><code class="js">const x = 1\n</code></pre>\n'
+
+        self.assertEqual(unsafe_markdown(text), html)
+        self.assertEqual(markdown(text), html)
